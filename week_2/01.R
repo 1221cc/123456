40660d0439e1fa33f9e3a0676ccbaaad4da4bbf8
1221cc <40812690+1221cc@users.noreply.github.com>
2018-07-11 13:06:45 +0800

    存01

--- week_2/01.R
+++ week_2/01.R
@@ -1,3 +1,19 @@
-taital:"01"
+library(ggplot2)
 
-iris
+#單變數:類別型
+ggplot(data = iris, aes(x= Petal.Length)) + geom_bar(fill = "lightyellow", colour = "black")
+
+#單變數:連續型
+ggplot(data = iris,aes(x = Petal.Length)) + geom_histogram()
+
+#雙變數:連續V.S.連續
+ggplot(data = iris,aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
+
+#雙變數:離散V.S.連續
+ggplot(iris,aes(x = Petal.Length, y = Petal.Width)) + geom_boxplot(colour = "black")
+
+
+
+
+
+
