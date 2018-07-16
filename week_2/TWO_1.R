d01fc8f8d10cd917dddfd21be19812d5c293e519
1221cc <40812690+1221cc@users.noreply.github.com>
2018-07-16 14:03:41 +0800

    修改TWO_1

--- week_2/TWO_1.R
+++ week_2/TWO_1.R
@@ -1,7 +1,8 @@
 library(ggplot2)
 
 #單變數:類別型
-ggplot(data = iris, aes(x= Petal.Length)) + geom_bar(fill = "lightyellow", colour = "black")
+ggplot(data = iris,aes(x= Species)) + geom_bar(fill = "lightblue", colour = "black")
+                                              
 
 #單變數:連續型
 ggplot(data = iris,aes(x = Petal.Length)) + geom_histogram()
@@ -10,7 +11,7 @@ ggplot(data = iris,aes(x = Petal.Length)) + geom_histogram()
 ggplot(data = iris,aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
 
 #雙變數:離散V.S.連續
-ggplot(iris,aes(x = Petal.Length, y = Petal.Width)) + geom_boxplot(colour = "black")
+ggplot(iris,aes(x = Species, y = Petal.Width)) + geom_boxplot(colour = "black")
 
 
 
