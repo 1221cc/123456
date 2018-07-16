cae48f266def0767c61f8b9eac7baec2f504ce97
1221cc <40812690+1221cc@users.noreply.github.com>
2018-07-16 15:03:11 +0800

    修改增加分析

--- week_2/TWO_1.R
+++ week_2/TWO_1.R
@@ -1,17 +1,61 @@
 library(ggplot2)
 
 #單變數:類別型
+##每一種個數各有幾個
 ggplot(data = iris,aes(x= Species)) + geom_bar(fill = "lightblue", colour = "black")
-                                              
+
+##花瓣長度各有幾個
+ggplot(data = iris,aes(x= Petal.Length)) + geom_bar(fill = "lightyellow", colour = "black")
+
+##花瓣寬度各有幾個
+ggplot(data = iris,aes(x= Petal.Width)) + geom_bar(fill = "lightpink", colour = "black")
+
+##花萼長度各有幾個
+ggplot(data = iris,aes(x= Sepal.Length)) + geom_bar(fill = "white", colour = "black")
+
+##花萼寬度各有幾個
+ggplot(data = iris,aes(x= Sepal.Width)) + geom_bar(fill = "violet", colour = "black")                                              
 
 #單變數:連續型
+##花瓣長度連續圖
 ggplot(data = iris,aes(x = Petal.Length)) + geom_histogram()
 
+##花瓣寬度連續圖
+ggplot(data = iris,aes(x = Petal.Width)) + geom_histogram()
+
+##花萼長度連續圖
+ggplot(data = iris,aes(x = Sepal.Length)) + geom_histogram()
+
+##花萼寬度連續圖
+ggplot(data = iris,aes(x = Sepal.Width)) + geom_histogram()
+
 #雙變數:連續V.S.連續
+
+##花瓣的長度與寬度分布
+ggplot(data = iris,aes(x = Petal.Length, y = Petal.Width)) + geom_point()
+
+##花萼的長度與寬度分布
 ggplot(data = iris,aes(x = Sepal.Length, y = Sepal.Width)) + geom_point()
 
+##種類與花瓣長分布
+ggplot(data = iris,aes(x = Species, y = Petal.Length)) + geom_point()
+
+##種類與花瓣寬分布
+ggplot(data = iris,aes(x = Species, y = Petal.Width)) + geom_point()
+
+##種類與花萼長分布
+ggplot(data = iris,aes(x = Species, y = Sepal.Length)) + geom_point()
+
+##種類與花萼寬分布
+ggplot(data = iris,aes(x = Species, y = Sepal.Width)) + geom_point()
+
+
 #雙變數:離散V.S.連續
-ggplot(iris,aes(x = Species, y = Petal.Width)) + geom_boxplot(colour = "black")
+##各種之花瓣長度箱型圖
+ggplot(iris,aes(x = Species, y = Petal.Length)) + geom_boxplot(colour = "black")
+
+##各種之花瓣長度小提琴圖
+ggplot(iris,aes(x = Species, y = Petal.Length)) + geom_violin()
 
 
 
